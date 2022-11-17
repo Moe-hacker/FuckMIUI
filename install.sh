16f913827a2da87e0b97386e90f6c3313e3c65a6
Moe-hacker <moe-hacker@outlook.com>
2022-11-17 23:02:44 +0800

    xxxx

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@ LATESTARTSERVICE=false
 POSTFSDATA=false
 PROPFILE=false
 print_modname() {
- printf "\033[33mFuckMIUI by Moe-hacker."
+ ui_print 'FuckMIUI by Moe-hacker.'
+ ui_print ''
  ui_print 'mmmmmm m    m   mmm  m    m m    m mmmmm  m    m mmmmm'
  ui_print '#      #    # m"   " #  m"  ##  ##   #    #    #   #'
  ui_print '#mmmmm #    # #      #m#    # ## #   #    #    #   #'
  ui_print '#      #    # #      #  #m  # "" #   #    #    #   #'
  ui_print '#      "mmmm"  "mmm" #   "m #    # mm#mm  "mmmm" mm#mm'
+ ui_print ''
  ui_print '愛にできることはまだあるよ，僕にできることはまだあるよ！'
 }
 on_install() {
