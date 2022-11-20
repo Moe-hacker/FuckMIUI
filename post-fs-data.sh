9bddf2cdf5ae7cc318857a4340a6c15c0706924b
沨鸾 <moe-hacker@outlook.com>
2022-11-20 19:29:01 +0800

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,5 +1,5 @@
 MODDIR=${0%/*}
-#手动挂载部分伪造的空文件
+#手动挂载部分伪造的替换文件
 #magiak貌似不认system_ext和vendor分区？(流汗滑稽)
 mount --bind $MODDIR/system_ext/priv-app/Polaris/Polaris.apk /system_ext/priv-app/Polaris/Polaris.apk
 mount --bind $MODDIR/system_ext/app/SecurityOnetrackService/SecurityOnetrackService.apk /system_ext/app/SecurityOnetrackService/SecurityOnetrackService.apk
