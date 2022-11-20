ba2cb68db13fab669bf5ae748c578095b9ad3a6d
Moe-hacker <moe-hacker@outlook.com>
2022-11-20 22:48:04 +0800

    xxxx

--- service.sh
+++ service.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#玄学优化没注释用途的部分来自网络，用途懒得添加了，反正也是玄学
+#玄学优化没注释用途的部分来自网络，用途懒得添加了，反正也是玄学，东西太多懒得测试，具体效果我也不知道，不过也没指望有效就是了
 LOG_FILE=/data/adb/modules/FuckMIUI/FuckMIUI.log
 BATTERY_LOG_FILE=/data/adb/modules/FuckMIUI/Battery.log
 #玄学优化
@@ -34,6 +34,15 @@ FuckMIUI(){
   #玄学优化
   service call SurfaceFlinger 1008 i32 1
   #玄学优化
+  echo 1 > /dev/stune/foreground/schedtune.prefer_idle
+  echo 1 > /dev/stune/background/schedtune.prefer_idle
+  echo 1 > /dev/stune/rt/schedtune.prefer_idle
+  echo 20 > /dev/stune/rt/schedtune.boost
+  echo 20 > /dev/stune/top-app/schedtune.boost
+  echo 1 > /dev/stune/schedtune.prefer_idle
+  echo 1 > /dev/stune/top-app/schedtune.prefer_idle
+  echo 50 > /dev/memcg/memory.swappiness
+  #玄学优化
   echo 1 > /dev/cpuset/sched_relax_domain_level
   echo 1 > /dev/cpuset/system-background/sched_relax_domain_level
   echo 1 > /dev/cpuset/background/sched_relax_domain_level
@@ -44,9 +53,19 @@ FuckMIUI(){
   for i in $(cat /data/adb/modules/FuckMIUI/nomerges.list);do
     echo 0 > $i
   done
-  #还是玄学优化
+  #还是玄学优化，甚至这段是两个地方找的缝合的
   cmd wifi force-low-latency-mode enabled
   echo 1 >/proc/sys/net/ipv4/tcp_low_latency
+  echo 2 > /proc/sys/net/ipv4/tcp_fin_timeout
+  echo 0 > /proc/sys/net/ipv4/tcp_keepalive_time
+  echo 2 > /proc/sys/net/ipv4/tcp_keepalive_probes
+  echo 2 > /proc/sys/net/ipv4/tcp_keepalive_intvl
+  echo 0 > /proc/sys/net/ipv4/tcp_timestamps
+  echo 3 > /proc/sys/net/ipv4/tcp_fastopen
+  echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
+  echo 0 > /proc/sys/net/ipv4/tcp_slow_start_after_idle
+  echo 1048576 > /proc/sys/net/core/rmem_default
+  echo 1048576 > /proc/sys/net/core/wmem_default
   #还™是玄学优化
   echo 0 > /sys/module/binder/parameters/debug_mask
   echo 0 > /sys/module/binder_alloc/parameters/debug_mask
