866017fa665d7f3f9564d7043a861b819db0b210
Moe-hacker <moe-hacker@outlook.com>
2022-11-22 15:34:51 +0800

    xxxx

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,3 +5,4 @@ MODDIR=${0%/*}
 mount --bind $MODDIR/system_ext/priv-app/Polaris/Polaris.apk /system_ext/priv-app/Polaris/Polaris.apk
 mount --bind $MODDIR/system_ext/app/SecurityOnetrackService/SecurityOnetrackService.apk /system_ext/app/SecurityOnetrackService/SecurityOnetrackService.apk
 mount --bind $MODDIR/vendor/bin/subsystem_ramdump /vendor/bin/subsystem_ramdump
+mount --bind $MODDIR/product/app/aiasst_service/aiasst_service.apk /product/app/aiasst_service/aiasst_service.apk
